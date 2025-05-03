98b1055cb3ef9c4624d69b437a43a2bdcaaf8827
yanecoder <deemanaumov@gmail.com>
2025-05-03 16:37:11 +0300

    dockers wrote

--- get/main.py
+++ get/main.py
@@ -9,13 +9,14 @@ def create_app():
     app.config.from_object(Config)
     db.init_app(app)
     
-    from .routes import main, send, get
-    app.register_blueprint(main)
-    app.register_blueprint(send)
+    from .routes import get
     app.register_blueprint(get)
 
-    
     with app.app_context():
         db.create_all()
         
     return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host="0.0.0.0", port=5000)
--- send/main.py
+++ send/main.py
@@ -9,13 +9,14 @@ def create_app():
     app.config.from_object(Config)
     db.init_app(app)
     
-    from .routes import main, send, get
-    app.register_blueprint(main)
+    from .routes import send
     app.register_blueprint(send)
-    app.register_blueprint(get)
-
     
     with app.app_context():
         db.create_all()
         
     return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host="0.0.0.0", port=4000)
